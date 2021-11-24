95893ff15a8ae54fff40270ec6270f6c31dc1ccb
Deseman <deseman@web.de>
2021-11-24 15:50:17 +0100

    disababled deployment

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8080;
+
+
+
+const helpers = require(`@staffbase/staffbase-plugin-sdk`).helpers;
+const publicKeyPath = './key';
+let keySecret;
+let tempTokenData;
+
+try {
+	keySecret = helpers.readKeyFile(publicKeyPath);
+    console.log(keySecret);
+} catch (err) {
+	console.log('Error Reading Key file', err);
+}
+
+const StaffBaseSSO = require('@staffbase/staffbase-plugin-sdk').sso;
+
+
+
+
+
+
+//----------------------------JSON ROUTES--------------------------------------------------------------------
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static
+
+app.get('/', function (req, res) {
+    const pluginId = 'staffbase.hrshowcase';
+    const publicKey = keySecret;
+    const jwtToken = req.query.jwt;
+    let tokenData = null;
+    
+    try {
+        let SSOContents = new StaffBaseSSO(pluginId, publicKey, jwtToken);
+        tokenData = SSOContents.getTokenData();
+        console.log('Received token data:', tokenData);
+        tempTokenData = tokenData;
+    } catch(tokenErr) {
+         console.error('Error decoding token:', tokenErr);
+    }
+      if (req.query.jwt) {
+         res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        // return res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    //     return res.send({ express: tokenData});
+        
+      }
+    // return res.send("Unable to open in Browser");
+    });
+
+    app.use('/', express.static(path.join(__dirname, 'build')));
+
+    // app.get('/', function (req, res) {
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // });
+
+
+    app.get('/api/hello', (req, res) => {
+        return res.send({ express: tempTokenData});
+    });
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+
+//------------------------------------------------------------------------------------------------
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // TODO: frontent URL
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('access-control-expose-headers', 'content-disposition');
+    next();
+});
+
+// routes
+app.get('/hw', (req, res) => res.send('Hello World!'));
+
+// error handling
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send('invalid token..');
+    }
+});
+
+// run server
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+
+
+
+
+//----------------------------AUTH--------------------------------------------------------------------
+
+
+// const helmet = require('helmet');
+
+// app.use(function (req, res, next) {
+//     res.setHeader(
+//       'Content-Security-Policy',
+//       "frame-ancestors 'self' http://iss-mvp-brazil.azurewebsites.net/ https://iss-mvp-brazil.azurewebsites.net/ http://staffbase.com localhost:*;"
+//     );
+//     next();
+//   });
+
+// app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function App() {
+
+class App extends Component {
+
+  
+
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  firstname;
+  lastname;
+  
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.text();
+    if (response.status !== 200) throw Error(body.message);
+    this.sliceFirstName(body);
+    this.sliceLastName(body);
+    return body;
+  };
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    this.setState({ responseToPost: body });
+  };
+  sliceFirstName = async (input) =>{
+    let position = input.search("given_name");
+    let position2 = input.search("family_name");
+    this.firstname  = input.slice(position+13, position2-3);
+  };
+  sliceLastName = async (input) =>{
+    let position = input.search("family_name");
+    let position2 = input.search("role");
+    this.lastname  = input.slice(position+14, position2-3);
+  };
+
+render() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <Form>
+  <Form.Group className="mvp-1" controlId="formBasicFirstName">
+    <Form.Label>First Name</Form.Label>
+    <Form.Control type="email" placeholder={this.firstname} />
+  </Form.Group>
+  <br></br>
+  <Form.Group className="mvp-1" controlId="formBasicLastName">
+    <Form.Label>Last Name</Form.Label>
+    <Form.Control type="password" placeholder={this.lastname} />
+  </Form.Group>
+  <br></br>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+  <br></br>
+  <Form.Text className="text-muted">
+      Data from Staffbase
+    </Form.Text>
+</Form>
+
+
+
+      {/* <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p> */}
+
     </div>
-  );
+  );}
 }
 
 export default App;
