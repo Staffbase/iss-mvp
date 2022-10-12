7aef10bf8d396f2d82a09e072df43dc733400b5b
Robin Deselaers <robin@staffbase.com>
2022-10-12 16:14:39 +0200

    modified:   README.md 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/assets/icon/favicon.png 	deleted:    public/assets/icon/icon.png 	deleted:    public/assets/shapes.svg 	new file:   public/favicon.ico 	modified:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	modified:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	deleted:    src/App.test.tsx 	deleted:    src/App.tsx 	deleted:    src/components/ExploreContainer.css 	deleted:    src/components/ExploreContainer.tsx 	new file:   src/index.css 	renamed:    src/index.tsx -> src/index.js 	new file:   src/logo.svg 	deleted:    src/pages/Tab1.css 	deleted:    src/pages/Tab1.tsx 	deleted:    src/pages/Tab2.css 	deleted:    src/pages/Tab2.tsx 	deleted:    src/react-app-env.d.ts 	renamed:    src/reportWebVitals.ts -> src/reportWebVitals.js 	deleted:    src/service-worker.ts 	deleted:    src/serviceWorkerRegistration.ts 	new file:   src/setupTests.js 	deleted:    src/setupTests.ts 	deleted:    src/theme/variables.css

---
+++ src/App.js
@@ -0,0 +1,263 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import Chart from "react-google-charts";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { BsPeople, BsBook} from "react-icons/bs";
+import { BiTime} from "react-icons/bi";
+import { MdOutlinePayments} from "react-icons/md";
+
+
+
+
+class App extends Component {
+
+  
+
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  firstname;
+  lastname;
+  tags;
+
+  state = { showing1: true };
+  state = { showing2: false };
+  state = { showing3: false };
+
+
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/hf89wnf89hw938h98hw');
+    const body = await response.text();
+    if (response.status !== 200) throw Error(body.message);
+    this.sliceFirstName(body);
+    this.sliceLastName(body);
+    return body;
+  };
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    this.setState({ responseToPost: body });
+  };
+  sliceFirstName = async (input) =>{
+    let position = input.search("given_name");
+    let position2 = input.search("family_name");
+    this.firstname  = input.slice(position+13, position2-3);
+  };
+  sliceLastName = async (input) =>{
+    let position = input.search("family_name");
+    let position2 = input.search("role");
+    this.lastname  = input.slice(position+14, position2-3);
+  };
+  sliceTags = async (input) =>{
+    let position = input.search("family_name");
+    let position2 = input.search("role");
+    this.tags  = input.slice(position, position2);
+  };
+
+
+
+render() {
+
+  const { showing1 } = this.state;
+  const { showing2 } = this.state;
+  const { showing3 } = this.state;
+  return (
+    <div className="App">
+
+      <header className="App-header">
+      <br></br>
+      <style type="text/css">
+    {`
+    .btn-flat {
+      background-color: #030139;
+      color: white;
+    }
+    `}
+  </style>
+      <ButtonGroup size="lg" className="mb-2">
+    <Button onClick={() => this.setState({ showing1: !showing1, showing2: false, showing3: false })} variant="flat" value="0"><BsPeople /></Button>
+    <Button onClick={() => this.setState({ showing2: !showing2, showing1: false, showing3: false })} variant="flat" value="1"><MdOutlinePayments /></Button>
+    <Button onClick={() => this.setState({ showing3: !showing3, showing1: false, showing2: false })} variant="flat" value="2"><BiTime /></Button>
+  </ButtonGroup>
+  
+      </header>
+      <br></br>
+
+      { showing1 
+                    ? <div>
+
+<Form>
+  <Form.Group className="mvp-1" controlId="formBasicFirstName">
+    <Form.Label>First Name</Form.Label>
+    <Form.Control type="text" value={this.firstname} placeholder={this.firstname} />
+  </Form.Group>
+  <br></br>
+  <Form.Group className="mvp-1" controlId="formBasicLastName">
+    <Form.Label>Last Name</Form.Label>
+    <Form.Control type="text" value={this.lastname} placeholder={this.lastname} />
+  </Form.Group>
+  <br></br>
+  <Form.Group controlId="formBasicFile" className="mvp-1">
+    <Form.Label>Approval Document</Form.Label>
+    <Form.Control type="file" />
+  </Form.Group>
+  <br></br>
+  <Button variant="flat" type="submit" >
+    Request Change
+  </Button>
+  <style type="text/css">
+    {`
+    .btn-flat {
+      background-color: #030139;
+      color: white;
+    }
+    `}
+  </style>
+  
+  <Form.Text className="text-muted">
+      
+    </Form.Text>
+</Form>
+
+  </div>
+                    : null
+                }
+
+{ showing2 
+                    ? <div>
+
+  <div className="App">
+      <div style={{ display: 'flex', maxWidth: 900 }}>
+      <Chart
+  width={'500px'}
+  height={'300px'}
+  chartType="PieChart"
+  loader={<div>Loading Chart</div>}
+  data={[
+    ['Part', 'Payment'],
+    ['Base', 4300],
+    ['Bonus', 1100],
+  ]}
+  options={{
+    title: 'Current Salary',
+    pieSliceText: 'value',
+    slices: {
+      0: { color: '#030139' },
+      1: { color: '#0583FE' },
+    },
+  }}
+  rootProps={{ 'data-testid': '1' }}
+/>
+
+</div>
+    </div>
+    <Form>
+    <Form.Group controlId="formFile" className="mb-3">
+    <Form.Select aria-label="Select Payslip">
+  <option>Select previous payslip</option>
+  <option value="1">July</option>
+  <option value="2">June</option>
+  <option value="3">March</option>
+</Form.Select>
+</Form.Group>
+<style type="text/css">
+    {`
+    .btn-flat {
+      background-color: #030139;
+      color: white;
+    }
+    `}
+  </style>
+
+  <br></br>
+  
+  <Button variant="flat" type="submit" onClick={(e) => {
+      e.preventDefault();
+      window.location.href='https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+      }}>
+    Access payslip
+  </Button>
+  </Form>
+
+  </div>
+                    : null
+                }
+
+{ showing3 
+                    ? <div>
+
+<Card ><br></br>
+      <Card.Title>Your next shift:</Card.Title>
+      <Card.Body>
+      <Card.Text>
+      Today, 9AM to 5PM
+    </Card.Text>
+    <style type="text/css">
+    {`
+    .btn-flat {
+      background-color: #030139;
+      color: white;
+    }
+    `}
+  </style>
+  <Button variant="flat" type="submit">
+    Clock In
+  </Button>
+</Card.Body>
+  </Card>
+  <br></br>
+  <Alert variant='info'>
+    Alex Smith has offered you a shift
+  </Alert>
+  <Alert variant='success'>
+    Your time off request was approved
+  </Alert>
+  <Alert variant='primary'>
+    You have a new shift!
+  </Alert>
+
+  </div>
+                    : null
+                }
+
+      {/* <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p> */}
+
+    </div>
+  );}
+}
+
+export default App;
